6e5b023d0479c9cab734e0352892c8ff64867873
Ivan Sozza <ivan.sozza04@gmail.com>
2024-11-10 17:17:17 +0100

    feat: live fetching with sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,12 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY })
 
   return (
     <>
@@ -31,6 +31,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
